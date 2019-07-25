931668ffffb2f9bafa27ae3782285ea5a7128fd8
deanfchung <deanthechung@gmail.com>
2019-07-24 21:15:10 -0700

    initial logo without styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
+const logo = require('../logo/overvue-logo.png')
 
 export const Header = () => {
 
   return (
-    <div>
-      
+    <div id = 'name'>
+      <img id = 'logo' src ={logo}/>
+      <br></br>
+      PreVue
     </div>
   )
 };
