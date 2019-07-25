f95cee97370d050b3ed9fca9156fd2f7ae1ff662
Dean Ohashi <dnohashi@Deans-MacBook-Pro-2.local>
2019-07-24 21:13:26 -0700

    added button styled component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import { Button } from './styled/Button';
 
 export const Header = () => {
 
   return (
     <div>
-      
+      <Button href="https://overvue-app.s3-us-west-2.amazonaws.com/OverVue-0.0.1.dmg">
+        hi
+      </Button>
     </div>
   )
 };
---
+++ src/components/styled/Button.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Button = styled.a`
+  font-size: 2rem;
+  padding: 10px 50px;
+  color: white;
+  background-color: black;
+  text-decoration: none;
+  border-radius: 5%;
+`;
