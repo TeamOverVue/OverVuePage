423b6e94f3908bb2a96b605beda7d4a8616d8df7
dnohashi <d.n.ohashi@gmail.com>
2019-07-25 00:03:31 -0700

    Revert "finished footer all dean"

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,27 +3,7 @@ import React from 'react';
 export const Footer = () => {
   return (
     <div>
-      <h3>THE TEAM</h3>
-      <DeveloperCard>
-        <img src="" className="dev-pic" />
-        <span className="dev-name"></span>
-        <a href="https://github.com/jeisele2" />
-      </DeveloperCard>
-      <DeveloperCard>
-        <img src="" className="dev-pic" />
-        <span className="dev-name"></span>
-        <a href="https://github.com/jeisele2" />
-      </DeveloperCard>
-      <DeveloperCard>
-        <img src="" className="dev-pic" />
-        <span className="dev-name"></span>
-        <a href="https://github.com/jeisele2" />
-      </DeveloperCard>
-      <DeveloperCard>
-        <img src="" className="dev-pic" />
-        <span className="dev-name"></span>
-        <a href="https://github.com/jeisele2" />
-      </DeveloperCard>
+      
     </div>
   )
 }
