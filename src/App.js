73ede48756bf27bcb6c60bcd3a3ade043489ef49
Joseph Eisele <eisele.joseph1@gmail.com>
2019-07-24 21:43:27 -0700

    added .env and footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Header } from './components/Header';
 import { Body } from './components/Body';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Body />
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import React from 'react';
 export const Footer = () => {
   return (
     <div>
+      <h3 className={'main-header'}>THE TEAM</h3>
       
     </div>
   )
