76a1554877c4b5de165dd2ab0854df4f47b8e4d7
Drew Nguyen <drew.ktran@gmail.com>
2019-07-24 22:40:47 -0700

    added changes to body

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Button } from './styled/Button';
-const logo = require('../logo/overvue-logo.png')
+import React from "react";
+import styled from "styled-components";
+import { Button } from "./styled/Button";
+const logo = require("../logo/overvue-logo.png");
 
 export const Header = () => {
   return (
-    <div>
-
-    <div id = 'name'>
-    <img id = 'logo' src ={logo}/>
-    <br></br>
-    OverVue
-  </div>
-  <div><p>All in one prototyping tool for Vue developers</p></div>
-  <br></br>
-  <div>
-      <Button className = {'button' }href="https://overvue-app.s3-us-west-2.amazonaws.com/OverVue-0.0.1.dmg">
-        Download for MacOS
-      </Button>
+    <div className="header">
+      <div id="name">
+        <img id="logo" src={logo} />
+        <br />
+        OverVue
+      </div>
+      <div>
+        <p>All in one prototyping tool for Vue developers</p>
+      </div>
+      <br />
+      <div>
+        <Button
+          className={"button"}
+          href="https://overvue-app.s3-us-west-2.amazonaws.com/OverVue-0.0.1.dmg"
+        >
+          Download for MacOS
+        </Button>
+      </div>
     </div>
-  </div>
-  )
+  );
 };
