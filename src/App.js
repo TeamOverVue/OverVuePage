018082b2e90bc0df5362f0c903c811d979b5776b
Dean Ohashi <dnohashi@Deans-MacBook-Pro-2.local>
2019-07-24 20:57:54 -0700

    Added components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Header } from './components/Header';
+import { Body } from './components/Body';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Body />
     </div>
   );
 }
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const Header = () => {
+export const Body = () => {
 
   return (
     <div>
