ae8b0df14a95b3bf0ce4028f169a45eb34360b73
Drew Nguyen <drew.ktran@gmail.com>
2019-07-24 21:15:19 -0700

    added h2 heading

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-
+import React from "react";
+import Page from "./components/Page";
 function App() {
   return (
     <div className="App">
-      
+      <Page />
     </div>
   );
 }
---
+++ src/components/Page.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import HeadTwoStyle from "./styles/HeadTwoStyle";
+
+export default class Page extends Component {
+  render() {
+    return (
+      <div>
+        <HeadTwoStyle>
+          <h2>this is a header</h2>
+        </HeadTwoStyle>
+      </div>
+    );
+  }
+}
