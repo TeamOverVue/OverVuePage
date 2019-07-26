c816563802b7c46ab0a97861aa99ad0f45c14f22
Drew Nguyen <drew.ktran@gmail.com>
2019-07-26 11:08:49 -0700

    media query for header

--- src/components/WebApp.js
+++ src/components/WebApp.js
@@ -3,12 +3,14 @@ import "../App.css";
 
 export const WebApp = () => {
   return (
-    <div className="margin-tb black" id="webapp">
+    <div className=" black" id="webapp">
       <h1>Demo 👇</h1>
-      <iframe
-        className="iframe"
-        src="https://dev.d3o6ss3lnctews.amplifyapp.com/#/"
-      />
+      <div class="m-1">
+        <iframe
+          className="iframe"
+          src="https://dev.d3o6ss3lnctews.amplifyapp.com/#/"
+        />
+      </div>
     </div>
   );
 };
