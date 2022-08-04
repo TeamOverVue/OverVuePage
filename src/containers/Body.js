c8a2e6a5d53d432b32e2f7cc15cd9b854dbc9a2c
Katherine <80862932+katherinek123@users.noreply.github.com>
2022-08-03 20:12:48 -0700

    fixed picture

--- src/containers/Body.js
+++ src/containers/Body.js
@@ -5,187 +5,188 @@ import styled from "styled-components";
 export const Body = () => {
   return (
     <div className="margin-tb-small">
-    <Section>
-      <div className="left">
-        <HeadTwoStyle>
-          <div>
-            <h1>Create Components</h1>
-            <p className="p">
-            Start off your project by creating and naming your first component. Interact with it in the CSS Container.
-            </p>
-          </div>
-        </HeadTwoStyle>
-        <img
-          alt = "Add html element Gif"
-          className="shadow"
-          src="https://media.giphy.com/media/MHqQTb5Vdtewlra4pG/giphy.gif"
-        />
-      </div>
-    </Section>
-    
-    <Section>
-      <div className="left">
-        <HeadTwoStyle>
-          <div>
-            <h1>Add and change HTML Elements</h1>
-            <p className="p">
-            Add HTML elements inside your component. You can now modify the specifications of your prototype.
-            </p>
-          </div>
-        </HeadTwoStyle>
-        <img
-          alt = "Add html element Gif"
-          className="shadow"
-          src="https://media.giphy.com/media/YyPpNuH86r25JZNd46/giphy.gif"
-        />
-      </div>
-    </Section>
+      <Section>
+        <div className="left">
+          <HeadTwoStyle>
+            <div>
+              <h1>Create Components</h1>
+              <p className="p">
+                Start off your project by creating and naming your first component. Interact with it in the CSS Container.
+              </p>
+            </div>
+          </HeadTwoStyle>
+          <img
+            alt="Add html element Gif"
+            className="shadow"
+            src="https://media.giphy.com/media/MHqQTb5Vdtewlra4pG/giphy.gif"
+          />
+        </div>
+      </Section>
 
-    <Section>
-      <div className="left">
-        <HeadTwoStyle>
-          <div>
-            <h1>Add attributes to components </h1>
-            <p className="p">
-            Add a class and/or an ID attribute to the component to tailor it to your needs. 
-            </p>
-          </div>
-        </HeadTwoStyle>
-        <img
-          alt = "Add component attribute Gif"
-          className="shadow"
-          src="https://media.giphy.com/media/3eKBo9Qfi16mVdGIz4/giphy.gif"
-        />
-      </div>
-    </Section>
+      <Section>
+        <div className="left">
+          <HeadTwoStyle>
+            <div>
+              <h1>Add and change HTML Elements</h1>
+              <p className="p">
+                Add HTML elements inside your component. You can now modify the specifications of your prototype.
+              </p>
+            </div>
+          </HeadTwoStyle>
+          <img
+            alt="Add html element Gif"
+            className="shadow"
+            src="https://media.giphy.com/media/YyPpNuH86r25JZNd46/giphy.gif"
+          />
+        </div>
+      </Section>
 
-    <Section>
-      <div className="left">
-        <HeadTwoStyle>
-          <div>
-            <h1>Add Notes</h1>
-            <p className="p">
-            You can add personalized messages to help you keep track of your components.
-            </p>
-          </div>
-        </HeadTwoStyle>
-        <img
-          alt = "Notes Gif"
-          className="shadow"
-          src="https://media.giphy.com/media/1ttTOYNOTYBSOoWtG1/giphy.gif"
-        />
-      </div>
-    </Section>
-  
-    <Section>
-      <div className="left">
-        <HeadTwoStyle>
-          <div>
-            <h1>Add Two-Way Binding or other Attributes</h1>
-            <p className="p">
-            Options to add attributes to add specific functionality such as two way binding and class is now available.
-            </p>
-          </div>
-        </HeadTwoStyle>
-        <img
-          alt = "Add html element attribute Gif"
-          className="shadow"
-          src="https://media.giphy.com/media/aXMkIStyTE0jkNlRtO/giphy.gif"
-        />
-      </div>
-    </Section>
+      <Section>
+        <div className="left">
+          <HeadTwoStyle>
+            <div>
+              <h1>Add attributes to components </h1>
+              <p className="p">
+                Add a class and/or an ID attribute to the component to tailor it to your needs.
+              </p>
+            </div>
+          </HeadTwoStyle>
+          <img
+            alt="Add component attribute Gif"
+            className="shadow"
+            src="https://media.giphy.com/media/3eKBo9Qfi16mVdGIz4/giphy.gif"
+          />
+        </div>
+      </Section>
 
-    <Section>
-      <div className="left">
-        <HeadTwoStyle>
-          <div>
-            <h1>Style HTML Elements</h1>
-            <p className="p">
-            Stylize HTML elements to visualize your product. Adjustable style options to include height, width, color, and size.
-            </p>
-          </div>
-        </HeadTwoStyle>
-        <img
-          alt = "Style html elements Gif"
-          className="shadow"
-          src="https://media.giphy.com/media/Q0DYBMnuMLXoo5buvW/giphy.gif"
-        />
-      </div>
-    </Section>
+      <Section>
+        <div className="left">
+          <HeadTwoStyle>
+            <div>
+              <h1>Add Notes</h1>
+              <p className="p">
+                You can add personalized messages to help you keep track of your components.
+              </p>
+            </div>
+          </HeadTwoStyle>
+          <img
+            alt="Notes Gif"
+            className="shadow"
+            src="https://media.giphy.com/media/1ttTOYNOTYBSOoWtG1/giphy.gif"
+          />
+        </div>
+      </Section>
 
-    <Section>
-      <div className="left">
-        <HeadTwoStyle>
-          <div>
-            <h1>Child Components</h1>
-            <p className="p">
-            Child components can be easily incorporated into the code snippet. Child components will be available to be added from the HTML element list. 
-            </p>
-          </div>
-        </HeadTwoStyle>
-        <img
-          alt = "Child components Gif"
-          className="shadow"
-          src="https://media.giphy.com/media/3j42CzkMDEpEFFbJZT/giphy.gif"
-        />
-      </div>
-    </Section>
+      <Section>
+        <div className="left">
+          <HeadTwoStyle>
+            <div>
+              <h1>Add Two-Way Binding or other Attributes</h1>
+              <p className="p">
+                Options to add attributes to add specific functionality such as two way binding and class is now available.
+              </p>
+            </div>
+          </HeadTwoStyle>
+          <img
+            alt="Add html element attribute Gif"
+            className="shadow"
+            src="https://media.giphy.com/media/aXMkIStyTE0jkNlRtO/giphy.gif"
+          />
+        </div>
+      </Section>
 
-    <Section>
-      <div className="left">
-        <HeadTwoStyle>
-          <div>
-            <h1>Visualize Prototype</h1>
-            <p className="p">
-            Note the project's high level overview by visiting the Project Tree tab to observe the component’s hierarchy. 
-            </p>
-          </div>
-        </HeadTwoStyle>
-        <img
-          alt = "Visualize prototype Gif"
-          className="shadow"
-          src="https://media.giphy.com/media/vncIbdxJ36uU7a1bk3/giphy.gif"
-        />
-      </div>
-    </Section>
+      <Section>
+        <div className="left">
+          <HeadTwoStyle>
+            <div>
+              <h1>Style HTML Elements</h1>
+              <p className="p">
+                Stylize HTML elements to visualize your product. Adjustable style options to include height, width, color, and size.
+              </p>
+            </div>
+          </HeadTwoStyle>
+          <img
+            alt="Style html elements Gif"
+            className="shadow"
+            src="https://media.giphy.com/media/Q0DYBMnuMLXoo5buvW/giphy.gif"
+          />
+        </div>
+      </Section>
 
-    <Section>
-      <div className="left">
-        <HeadTwoStyle>
-          <div>
-            <h1>Code Snippet</h1>
-            <p className="p">
-            The boilerplate code is visible at every step of prototyping and dynamically updates according to new edits.
-            </p>
-          </div>
-        </HeadTwoStyle>
-        <img
-          alt = "Code snippet Gif"
-          className="shadow"
-          src="https://media.giphy.com/media/JLCUOmKfQpOmdWUf4l/giphy.gif"
-        />
-      </div>
-    </Section>
+      <Section>
+        <div className="left">
+          <HeadTwoStyle>
+            <div>
+              <h1>Child Components</h1>
+              <p className="p">
+                Child components can be easily incorporated into the code snippet. Child components will be available to be added from the HTML element list.
+              </p>
+            </div>
+          </HeadTwoStyle>
+          <img
+            alt="Child components Gif"
+            className="shadow"
+            src="https://media.giphy.com/media/3j42CzkMDEpEFFbJZT/giphy.gif"
+          />
+        </div>
+      </Section>
 
-    <Section>
-      <div className="left">
-        <HeadTwoStyle>
-          <div>
-            <h1>Export boilerplate</h1>
-            <p className="p">
-            Export the prototype's code in a lightweight boilerplate and develop the product further in an IDE. 
-            </p>
-          </div>
-        </HeadTwoStyle>
-        <img
-          alt = "Export boilerplate Gif"
-          className="shadow docker-img"
-          src="https://media.giphy.com/media/v8IbYTVrgFboHgpxBB/giphy.gif"
-        />
-      </div>
-    </Section>
-  </div>
-)};
+      <Section>
+        <div className="left">
+          <HeadTwoStyle>
+            <div>
+              <h1>Visualize Prototype</h1>
+              <p className="p">
+                Note the project's high level overview by visiting the Project Tree tab to observe the component’s hierarchy.
+              </p>
+            </div>
+          </HeadTwoStyle>
+          <img
+            alt="Visualize prototype Gif"
+            className="shadow"
+            src="https://media.giphy.com/media/vncIbdxJ36uU7a1bk3/giphy.gif"
+          />
+        </div>
+      </Section>
+
+      <Section>
+        <div className="left">
+          <HeadTwoStyle>
+            <div>
+              <h1>Code Snippet</h1>
+              <p className="p">
+                The boilerplate code is visible at every step of prototyping and dynamically updates according to new edits.
+              </p>
+            </div>
+          </HeadTwoStyle>
+          <img
+            alt="Code snippet Gif"
+            className="shadow"
+            src="https://media.giphy.com/media/JLCUOmKfQpOmdWUf4l/giphy.gif"
+          />
+        </div>
+      </Section>
+
+      <Section>
+        <div className="left">
+          <HeadTwoStyle>
+            <div>
+              <h1>Export boilerplate</h1>
+              <p className="p">
+                Export the prototype's code in a lightweight boilerplate and develop the product further in an IDE.
+              </p>
+            </div>
+          </HeadTwoStyle>
+          <img
+            alt="Export boilerplate Gif"
+            className="shadow"
+            src="https://media.giphy.com/media/v8IbYTVrgFboHgpxBB/giphy.gif"
+          />
+        </div>
+      </Section>
+    </div>
+  )
+};
 
 const Section = styled.div`
   /* mobile */
