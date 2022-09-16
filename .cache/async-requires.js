6852ecf838241cda92a717b55a3bbd700b991db5
Alex Law <alexlaw528@gmail.com>
2022-09-16 10:37:46 -0700

    Update dev pictures for OverVue 8.0.
    Remove Footer component.

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -1,8 +1,44 @@
 exports.components = {
-  "component---src-pages-docs-core-features-js": () => import("./../../../src/pages/docs/core-features.js" /* webpackChunkName: "component---src-pages-docs-core-features-js" */),
-  "component---src-pages-docs-index-js": () => import("./../../../src/pages/docs/index.js" /* webpackChunkName: "component---src-pages-docs-index-js" */),
-  "component---src-pages-docs-installation-js": () => import("./../../../src/pages/docs/installation.js" /* webpackChunkName: "component---src-pages-docs-installation-js" */),
-  "component---src-pages-examples-index-js": () => import("./../../../src/pages/examples/index.js" /* webpackChunkName: "component---src-pages-examples-index-js" */),
-  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=default" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-docs-canvas-customization-js": () => import("./../../../src/pages/docs/canvas-customization.js?export=default" /* webpackChunkName: "component---src-pages-docs-canvas-customization-js" */),
+  "component---src-pages-docs-component-attributes-js": () => import("./../../../src/pages/docs/component-attributes.js?export=default" /* webpackChunkName: "component---src-pages-docs-component-attributes-js" */),
+  "component---src-pages-docs-component-libraries-js": () => import("./../../../src/pages/docs/component-libraries.js?export=default" /* webpackChunkName: "component---src-pages-docs-component-libraries-js" */),
+  "component---src-pages-docs-component-styling-js": () => import("./../../../src/pages/docs/component-styling.js?export=default" /* webpackChunkName: "component---src-pages-docs-component-styling-js" */),
+  "component---src-pages-docs-core-features-js": () => import("./../../../src/pages/docs/core-features.js?export=default" /* webpackChunkName: "component---src-pages-docs-core-features-js" */),
+  "component---src-pages-docs-create-component-js": () => import("./../../../src/pages/docs/create-component.js?export=default" /* webpackChunkName: "component---src-pages-docs-create-component-js" */),
+  "component---src-pages-docs-delete-component-js": () => import("./../../../src/pages/docs/delete-component.js?export=default" /* webpackChunkName: "component---src-pages-docs-delete-component-js" */),
+  "component---src-pages-docs-export-js": () => import("./../../../src/pages/docs/export.js?export=default" /* webpackChunkName: "component---src-pages-docs-export-js" */),
+  "component---src-pages-docs-hierarchy-visualization-js": () => import("./../../../src/pages/docs/hierarchy-visualization.js?export=default" /* webpackChunkName: "component---src-pages-docs-hierarchy-visualization-js" */),
+  "component---src-pages-docs-import-js": () => import("./../../../src/pages/docs/import.js?export=default" /* webpackChunkName: "component---src-pages-docs-import-js" */),
+  "component---src-pages-docs-index-js": () => import("./../../../src/pages/docs/index.js?export=default" /* webpackChunkName: "component---src-pages-docs-index-js" */),
+  "component---src-pages-docs-installation-js": () => import("./../../../src/pages/docs/installation.js?export=default" /* webpackChunkName: "component---src-pages-docs-installation-js" */),
+  "component---src-pages-docs-routes-js": () => import("./../../../src/pages/docs/routes.js?export=default" /* webpackChunkName: "component---src-pages-docs-routes-js" */),
+  "component---src-pages-docs-slack-login-js": () => import("./../../../src/pages/docs/slack-login.js?export=default" /* webpackChunkName: "component---src-pages-docs-slack-login-js" */),
+  "component---src-pages-docs-typescript-js": () => import("./../../../src/pages/docs/typescript.js?export=default" /* webpackChunkName: "component---src-pages-docs-typescript-js" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js?export=default" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-ourteam-index-js": () => import("./../../../src/pages/ourteam/index.js?export=default" /* webpackChunkName: "component---src-pages-ourteam-index-js" */)
+}
+
+
+
+exports.head = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=head" /* webpackChunkName: "component---cache-dev-404-page-jshead" */),
+  "component---src-pages-docs-canvas-customization-js": () => import("./../../../src/pages/docs/canvas-customization.js?export=head" /* webpackChunkName: "component---src-pages-docs-canvas-customization-jshead" */),
+  "component---src-pages-docs-component-attributes-js": () => import("./../../../src/pages/docs/component-attributes.js?export=head" /* webpackChunkName: "component---src-pages-docs-component-attributes-jshead" */),
+  "component---src-pages-docs-component-libraries-js": () => import("./../../../src/pages/docs/component-libraries.js?export=head" /* webpackChunkName: "component---src-pages-docs-component-libraries-jshead" */),
+  "component---src-pages-docs-component-styling-js": () => import("./../../../src/pages/docs/component-styling.js?export=head" /* webpackChunkName: "component---src-pages-docs-component-styling-jshead" */),
+  "component---src-pages-docs-core-features-js": () => import("./../../../src/pages/docs/core-features.js?export=head" /* webpackChunkName: "component---src-pages-docs-core-features-jshead" */),
+  "component---src-pages-docs-create-component-js": () => import("./../../../src/pages/docs/create-component.js?export=head" /* webpackChunkName: "component---src-pages-docs-create-component-jshead" */),
+  "component---src-pages-docs-delete-component-js": () => import("./../../../src/pages/docs/delete-component.js?export=head" /* webpackChunkName: "component---src-pages-docs-delete-component-jshead" */),
+  "component---src-pages-docs-export-js": () => import("./../../../src/pages/docs/export.js?export=head" /* webpackChunkName: "component---src-pages-docs-export-jshead" */),
+  "component---src-pages-docs-hierarchy-visualization-js": () => import("./../../../src/pages/docs/hierarchy-visualization.js?export=head" /* webpackChunkName: "component---src-pages-docs-hierarchy-visualization-jshead" */),
+  "component---src-pages-docs-import-js": () => import("./../../../src/pages/docs/import.js?export=head" /* webpackChunkName: "component---src-pages-docs-import-jshead" */),
+  "component---src-pages-docs-index-js": () => import("./../../../src/pages/docs/index.js?export=head" /* webpackChunkName: "component---src-pages-docs-index-jshead" */),
+  "component---src-pages-docs-installation-js": () => import("./../../../src/pages/docs/installation.js?export=head" /* webpackChunkName: "component---src-pages-docs-installation-jshead" */),
+  "component---src-pages-docs-routes-js": () => import("./../../../src/pages/docs/routes.js?export=head" /* webpackChunkName: "component---src-pages-docs-routes-jshead" */),
+  "component---src-pages-docs-slack-login-js": () => import("./../../../src/pages/docs/slack-login.js?export=head" /* webpackChunkName: "component---src-pages-docs-slack-login-jshead" */),
+  "component---src-pages-docs-typescript-js": () => import("./../../../src/pages/docs/typescript.js?export=head" /* webpackChunkName: "component---src-pages-docs-typescript-jshead" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js?export=head" /* webpackChunkName: "component---src-pages-index-jshead" */),
+  "component---src-pages-ourteam-index-js": () => import("./../../../src/pages/ourteam/index.js?export=head" /* webpackChunkName: "component---src-pages-ourteam-index-jshead" */)
 }
 
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,10 +4,23 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/alaw/Codesmith/full-imersive/osp/OverVuePage/.cache/dev-404-page.js")),
+  "component---src-pages-docs-canvas-customization-js": preferDefault(require("/Users/alaw/Codesmith/full-imersive/osp/OverVuePage/src/pages/docs/canvas-customization.js")),
+  "component---src-pages-docs-component-attributes-js": preferDefault(require("/Users/alaw/Codesmith/full-imersive/osp/OverVuePage/src/pages/docs/component-attributes.js")),
+  "component---src-pages-docs-component-libraries-js": preferDefault(require("/Users/alaw/Codesmith/full-imersive/osp/OverVuePage/src/pages/docs/component-libraries.js")),
+  "component---src-pages-docs-component-styling-js": preferDefault(require("/Users/alaw/Codesmith/full-imersive/osp/OverVuePage/src/pages/docs/component-styling.js")),
   "component---src-pages-docs-core-features-js": preferDefault(require("/Users/alaw/Codesmith/full-imersive/osp/OverVuePage/src/pages/docs/core-features.js")),
+  "component---src-pages-docs-create-component-js": preferDefault(require("/Users/alaw/Codesmith/full-imersive/osp/OverVuePage/src/pages/docs/create-component.js")),
+  "component---src-pages-docs-delete-component-js": preferDefault(require("/Users/alaw/Codesmith/full-imersive/osp/OverVuePage/src/pages/docs/delete-component.js")),
+  "component---src-pages-docs-export-js": preferDefault(require("/Users/alaw/Codesmith/full-imersive/osp/OverVuePage/src/pages/docs/export.js")),
+  "component---src-pages-docs-hierarchy-visualization-js": preferDefault(require("/Users/alaw/Codesmith/full-imersive/osp/OverVuePage/src/pages/docs/hierarchy-visualization.js")),
+  "component---src-pages-docs-import-js": preferDefault(require("/Users/alaw/Codesmith/full-imersive/osp/OverVuePage/src/pages/docs/import.js")),
   "component---src-pages-docs-index-js": preferDefault(require("/Users/alaw/Codesmith/full-imersive/osp/OverVuePage/src/pages/docs/index.js")),
   "component---src-pages-docs-installation-js": preferDefault(require("/Users/alaw/Codesmith/full-imersive/osp/OverVuePage/src/pages/docs/installation.js")),
-  "component---src-pages-examples-index-js": preferDefault(require("/Users/alaw/Codesmith/full-imersive/osp/OverVuePage/src/pages/examples/index.js")),
-  "component---src-pages-index-js": preferDefault(require("/Users/alaw/Codesmith/full-imersive/osp/OverVuePage/src/pages/index.js"))
+  "component---src-pages-docs-routes-js": preferDefault(require("/Users/alaw/Codesmith/full-imersive/osp/OverVuePage/src/pages/docs/routes.js")),
+  "component---src-pages-docs-slack-login-js": preferDefault(require("/Users/alaw/Codesmith/full-imersive/osp/OverVuePage/src/pages/docs/slack-login.js")),
+  "component---src-pages-docs-typescript-js": preferDefault(require("/Users/alaw/Codesmith/full-imersive/osp/OverVuePage/src/pages/docs/typescript.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/alaw/Codesmith/full-imersive/osp/OverVuePage/src/pages/index.js")),
+  "component---src-pages-ourteam-index-js": preferDefault(require("/Users/alaw/Codesmith/full-imersive/osp/OverVuePage/src/pages/ourteam/index.js"))
 }
 
